a0e311a62b25e2f6682988b7bb46dd0f07782a4f
tzvaita <tzvaita@gmail.com>
2019-04-26 19:56:05 +0200

    migrations done

---
+++ db/migrate/20190414110659_create_stories.rb
@@ -0,0 +1,10 @@
+class CreateStories < ActiveRecord::Migration[5.2]
+  def change
+    create_table :stories, force:true do |t|
+      t.string :name
+      t.string :link
+
+      t.timestamps
+    end
+  end
+end
